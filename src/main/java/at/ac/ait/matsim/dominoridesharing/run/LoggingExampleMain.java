5de37411237b17ba8ab0db314129a2912554255e
Markus Straub <markus.straub@ait.ac.at>
2022-12-23 11:10:01 +0100

    log4j2

--- src/main/java/at/ac/ait/matsim/dominoridesharing/run/LoggingExampleMain.java
+++ src/main/java/at/ac/ait/matsim/dominoridesharing/run/LoggingExampleMain.java
@@ -1,19 +1,24 @@
 package at.ac.ait.matsim.dominoridesharing.run;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class LoggingExampleMain {
 
-    private static Logger LOGGER = Logger.getLogger(LoggingExampleMain.class);
+    private static Logger LOGGER = LogManager.getLogger();
 
     public static void main(String[] args) {
         LOGGER.warn("this is a warning!");
         LOGGER.debug("we are going to call echomethod now..");
-        echoMethod(10);
+        int data = 10;
+        int echoResult = echoMethod(data);
+
+        // note the curly brace syntax:
+        LOGGER.info("we passed {} to the echMethod and got {} in return.", data, echoResult);
+
     }
 
     public static int echoMethod(int data) {
-        LOGGER.info("we got some data: " + data);
         return data;
     }
 
