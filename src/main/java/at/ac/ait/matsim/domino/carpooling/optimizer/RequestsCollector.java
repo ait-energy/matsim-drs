d647528ee0508cfa4d7901cc9aa44f93ea958ec9
Markus Straub <markus.straub@ait.ac.at>
2023-04-13 13:41:58 +0200

    avoid NPE

--- src/main/java/at/ac/ait/matsim/domino/carpooling/optimizer/RequestsCollector.java
+++ src/main/java/at/ac/ait/matsim/domino/carpooling/optimizer/RequestsCollector.java
@@ -1,16 +1,27 @@
 package at.ac.ait.matsim.domino.carpooling.optimizer;
 
-import at.ac.ait.matsim.domino.carpooling.run.Carpooling;
-import at.ac.ait.matsim.domino.carpooling.request.CarpoolingRequest;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.matsim.api.core.v01.Id;
 import org.matsim.api.core.v01.network.Link;
 import org.matsim.api.core.v01.network.Network;
-import org.matsim.api.core.v01.population.*;
+import org.matsim.api.core.v01.population.Activity;
+import org.matsim.api.core.v01.population.Leg;
+import org.matsim.api.core.v01.population.Person;
+import org.matsim.api.core.v01.population.Population;
 import org.matsim.contrib.dvrp.optimizer.Request;
 import org.matsim.core.router.TripStructureUtils;
-import java.util.*;
+
+import at.ac.ait.matsim.domino.carpooling.request.CarpoolingRequest;
+import at.ac.ait.matsim.domino.carpooling.run.Carpooling;
 
 public class RequestsCollector {
+    private static final Logger LOGGER = LogManager.getLogger();
+
     private final List<CarpoolingRequest> driversRequests;
     private final List<CarpoolingRequest> ridersRequests;
     private final Population population;
@@ -33,23 +44,21 @@ public class RequestsCollector {
                 List<Leg> legs = trip.getLegsOnly();
                 for (Leg leg : legs) {
                     String mode = leg.getMode();
+                    Activity startActivity = trip.getOriginActivity();
+                    Link fromLink = network.getLinks().get(startActivity.getLinkId());
+                    Activity endActivity = trip.getDestinationActivity();
+                    Link toLink = network.getLinks().get(endActivity.getLinkId());
+                    double activityEndTime = startActivity.getEndTime().seconds();
+                    if (fromLink == null || toLink == null) {
+                        LOGGER.warn("null link for start or end activity of leg for person {}.", person.getId());
+                        continue;
+                    }
                     if (mode.equals(Carpooling.RIDER_MODE)) {
-                        Activity startActivity = trip.getOriginActivity();
-                        Link fromLink = network.getLinks().get(startActivity.getLinkId());
-                        Activity endActivity = trip.getDestinationActivity();
-                        Link toLink = network.getLinks().get(endActivity.getLinkId());
-                        double activityEndTime = startActivity.getEndTime().seconds();
                         riderRequestID = riderRequestID + 1;
                         CarpoolingRequest riderRequest = new CarpoolingRequest(Id.create(riderRequestID, Request.class),
                                 person, trip, activityEndTime, mode, fromLink, toLink, leg);
                         ridersRequests.add(riderRequest);
-                    }
-                    if (mode.equals(Carpooling.DRIVER_MODE)) {
-                        Activity startActivity = trip.getOriginActivity();
-                        Link fromLink = network.getLinks().get(startActivity.getLinkId());
-                        Activity endActivity = trip.getDestinationActivity();
-                        Link toLink = network.getLinks().get(endActivity.getLinkId());
-                        double activityEndTime = startActivity.getEndTime().seconds();
+                    } else if (mode.equals(Carpooling.DRIVER_MODE)) {
                         driverRequestID = driverRequestID + 1;
                         CarpoolingRequest driverRequest = new CarpoolingRequest(
                                 Id.create(driverRequestID, Request.class), person, trip, activityEndTime, mode,
