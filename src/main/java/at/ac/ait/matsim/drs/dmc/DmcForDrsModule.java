94bfc51b578bef08b2ddcd20c6291f39c6ab7d40
Markus Straub <markus.straub@ait.ac.at>
2025-01-16 14:09:47 +0100

    add missing provider, adjust config.. it even works.. but why?

--- src/main/java/at/ac/ait/matsim/drs/dmc/DmcForDrsModule.java
+++ src/main/java/at/ac/ait/matsim/drs/dmc/DmcForDrsModule.java
@@ -1,6 +1,10 @@
 package at.ac.ait.matsim.drs.dmc;
 
 import org.matsim.contribs.discrete_mode_choice.modules.AbstractDiscreteModeChoiceExtension;
+import org.matsim.contribs.discrete_mode_choice.modules.config.DiscreteModeChoiceConfigGroup;
+import org.matsim.contribs.discrete_mode_choice.modules.config.ModeAvailabilityConfigGroup;
+
+import com.google.inject.Provides;
 
 public class DmcForDrsModule extends AbstractDiscreteModeChoiceExtension {
 
@@ -9,6 +13,13 @@ public class DmcForDrsModule extends AbstractDiscreteModeChoiceExtension {
     @Override
     protected void installExtension() {
         bindModeAvailability(CAR_AND_DRSDRIVER).to(CarAndDrsDriverModeAvailability.class);
+    }
+
+    @Provides
+    public CarAndDrsDriverModeAvailability provideCarAndDrsDriverModeAvailability(
+            DiscreteModeChoiceConfigGroup dmcConfig) {
+        ModeAvailabilityConfigGroup config = dmcConfig.getCarModeAvailabilityConfig();
+        return new CarAndDrsDriverModeAvailability(config.getAvailableModes());
     }
 
 }
